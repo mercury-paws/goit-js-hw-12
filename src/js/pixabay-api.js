37a5c02f78473dbeb89d252871380160ff334f17
mercury-paws <145927427+mercury-paws@users.noreply.github.com>
2024-03-03 15:08:48 +0100

    js 12 hm 2

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,29 +1,21 @@
 import axios from 'axios';
 
-export function fetchImages(q) {
+export async function fetchImages(q) {
   let baseURL = 'https://pixabay.com/api/';
-  return axios
-    .get(baseURL, {
-      params: {
-        key: '42490410-91d7fda5db61c0a899b50e1d9',
-        q,
-        // per_page: 15,
-        // page: 1,
-        orientation: 'horizontal',
-        safesearch: true,
-      },
-    })
-    .then(response => {
-      console.log(response.data);
-      if (response.status !== 200) {
-        throw new Error('Image error!');
-      }
-      const Data = response.data;
-      return Data;
-    });
+  const response = await axios.get(baseURL, {
+    params: {
+      key: '42490410-91d7fda5db61c0a899b50e1d9',
+      q,
+      per_page: 15,
+      // page: 1,
+      orientation: 'horizontal',
+      safesearch: true,
+    },
+  });
+
+  if (response.status !== 200) {
+    throw new Error('Image error!');
+  }
+  const Data = await response.data;
+  return Data.hits;
 }
-
-// https://pixabay.com/api/?key=42490410-91d7fda5db61c0a899b50e1d9&q=fox&orientation=horizontal&safesearch=true
-// https://pixabay.com/api/?KEY=42490410-91d7fda5db61c0a899b50e1d9&q=fox&ORIENTATION=horizontal&SAFESEARCH=true
-
-// https://pixabay.com/api/?KEY=42490410-91d7fda5db61c0a899b50e1d9&ORIENTATION=horizontal&SAFESEARCH=true&query=fox
--- src/main.js
+++ src/main.js
@@ -10,8 +10,10 @@ import { displayImages } from './js/render-functions.js';
 const inputSearch = document.querySelector('.input-picstyle');
 const searchButton = document.querySelector('.search-btn');
 const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.loadmore-btn');
+let page;
 
-searchButton.addEventListener('click', () => {
+searchButton.addEventListener('click', async () => {
   const q = inputSearch.value.trim().split(' ').join('+');
   console.log(q);
   if (!q) {
@@ -25,39 +27,46 @@ searchButton.addEventListener('click', () => {
     return;
   }
   gallery.innerHTML = '<span class="loader"></span>';
-  fetchImages(q)
-    .then(Data => {
-      const images = Data.hits;
-      console.log(images);
-      if (images.length === 0) {
-        iziToast.error({
-          color: 'yellow',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          position: 'center',
-          progressBarColor: 'rgb(0, 255, 184)',
-          timeout: 2000,
-        });
-      }
+
+  try {
+    const images = await fetchImages(q);
+    if (images.length === 0) {
+      iziToast.error({
+        color: 'yellow',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        position: 'center',
+        progressBarColor: 'rgb(0, 255, 184)',
+        timeout: 2000,
+      });
+    } else {
       displayImages(images);
+      page = 1;
+      loadMoreBtn.classList.remove('is-hidden');
       let lightboxInstance = new SimpleLightbox('.pic-card a', {
         captionsData: 'alt',
         captionDelay: 250,
       });
       lightboxInstance.refresh();
-    })
-    .catch(error => {
-      iziToast.error({
-        color: 'red',
-        message: 'Error while fetching images',
-        position: 'center',
-        progressBarColor: 'rgb(0, 255, 184)',
-        timeout: 2000,
-      });
-    })
-    .finally(() => {
-      inputSearch.value = '';
-      let lastQuery = q.split('+').join(' ');
-      inputSearch.placeholder = `Last searched for "${lastQuery}"`;
+    }
+  } catch (error) {
+    console.log('Error caught:', error.message);
+    iziToast.error({
+      color: 'red',
+      message: 'Error while fetching images',
+      position: 'center',
+      progressBarColor: 'rgb(0, 255, 184)',
+      timeout: 2000,
     });
+  } finally {
+    inputSearch.value = '';
+    let lastQuery = q.split('+').join(' ');
+    inputSearch.placeholder = `Last searched for "${lastQuery}"`;
+  }
 });
+
+// loadMoreBtn.addEventListener('click', event => {
+//   page++;
+//   const q = inputSearch.value.trim().split(' ').join('+');
+//   fetchImages(q);
+// });
